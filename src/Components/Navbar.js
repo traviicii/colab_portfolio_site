9d14bfeb92711d732dab1139c0354c17b43d6fea
traviicii <125103256+traviicii@users.noreply.github.com>
2023-09-04 22:53:20 -0400

    Altered navbar styling and began work on projects page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,13 +5,15 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
 
   return (
+    <div id='navbar'>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        {/* <Link to="/" id='navlinks'>Home</Link> */}
+        <Link to="/about" id='navlinks'>About</Link>
+        <Link to='/projects' id='navlinks'>Projects</Link>
+        <Link to='/inspiration' id='navlinks'>Inspiration</Link>
+      </div>
+    </div>
 
-        <div id='navbar'>
-          <Link to="/" id='navlinks'>Home</Link>
-          <Link to="/about" id='navlinks'>About</Link>
-          <Link to='/projects' id='navlinks'>Projects</Link>
-          <Link to='/inspiration' id='navlinks'>Inspiration</Link>
-        </div>
 
   )
 }
--- src/Views/Projects.jsx
+++ src/Views/Projects.jsx
@@ -3,9 +3,18 @@ import './Main.css'
 
 export default function Projects() {
   return (
-    <div id='split'>
-        <div>
-            <p>Projects</p>Projects
+    <div className='projects' id='split'>
+        <div className='projectsblocks'>
+            <p>myGuest</p>
+            <p>
+                myGuest is my most recent project, and was also the capstone project I created in order to graduate from Coding Templ's software engineering bootcamp. It is a full stack, fully responsive web application designed for hairstylists. It allows you to keep a detailed custom database
+            </p>
+        </div>
+        <div className='projectsblocks'>
+            <p>Poke Battle!</p>
+        </div>
+        <div className='projectsblocks'>
+            <p>Traviicii Weather</p>
         </div>
       </div>
   )
