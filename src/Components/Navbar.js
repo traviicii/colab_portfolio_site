660166a5d4b5dc03c80445583a5733fd763c2b83
traviicii <125103256+traviicii@users.noreply.github.com>
2025-01-09 18:28:11 -0500

    Added animation to navbar links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 export default function Navbar() {
 
+  gsap.registerPlugin(useGSAP);
+
+  const container = useRef()
+
+  useGSAP(
+    () => {
+      // rise up entrance
+      gsap.from('.link1', { duration: 1.2, y: 300, rotation: 260, ease: "back", backgroundColor: "black" })
+  
+      // dangle entrance
+      gsap.from('.link2', { duration: 2.6, x: -300, rotation: 90, ease: "elastic" })
+  
+      // drop in entrance
+      gsap.from('.link3', { duration: 1.7, y: -300, rotation: 270, ease: "back" })
+
+
+      gsap.from('#navbar', { duration: 1, opacity: 0, ease: 'power1.in'}
+
+      
+      , 
+      { scope: container })
+    })
+
   return (
-    <div className='ptsans' id='navbar'>
-      <div style={{display: "flex", justifyContent: "center"}}>
-        {/* <Link to="/" id='navlinks'>Home</Link> */}
-        <Link to="/about" id='navlinks'>About</Link>
-        <Link to='/projects' id='navlinks'>Projects</Link>
-        <Link to='/inspiration' id='navlinks'>Inspiration</Link>
+    <div className='ptsans' id='navbar' ref={container}>
+      <div style={{ display: "flex", justifyContent: "center" }} id='linkBox'>
+        <Link to="/about" id='navlinks' className='link1'>About</Link>
+        <Link to='/projects' id='navlinks' className='link2'>Projects</Link>
+        <Link to='/inspiration' id='navlinks' className='link3'>Inspiration</Link>
       </div>
     </div>
 
 
-  )
-}
-
+  )}
